b0a354a9d73c254ff7f6fb2f9641ae52f3aba7ad
flowd <baimless.club@gmail.com>
2022-01-01 03:28:04 -0300

    :rocket: STB 1.0 deploy
    STB 1.0 deploy

---
+++ README.md
@@ -0,0 +1,23 @@
+[![Github All Releases](https://img.shields.io/github/downloads/flooowd/steam-hour-boost/total.svg)]()
+[![Github Stars](https://img.shields.io/github/stars/flooowd/steam-hour-boost)]()
+[![Github Forks](https://img.shields.io/github/forks/flooowd/steam-hour-boost)]()
+[![Github Issues](https://img.shields.io/github/issues/flooowd/steam-hour-boost)]()
+
+# 🚀 Steam Hour Boost - Free & Open Source Hour Boost
+
+### [Download Here](https://github.com/flooowd/steam-hour-boost/releases) the compiled version.
+
+[![Terminal](https://cdn.upload.systems/uploads/rT91Ga90.png)]()
+
+
+
+## Or compile yourself using the code below
+
+```console
+git clone https://github.com/flooowd/steam-hour-boost.git
+cd steam-hour-boost-main
+pip install -r requirements.txt
+cd src
+pyinstaller --noconfirm --onefile --console  "./main.py"
+```
+
---
+++ src/main.py
@@ -0,0 +1,126 @@
+from steam.client import SteamClient
+from stdiomask import getpass
+from os import system
+from time import sleep
+
+import sys
+import requests as req
+from steam.core.msg import MsgProto
+from steam.enums.emsg import EMsg
+
+ascii_art = """
+   ____________________    __  ___                          
+  / ___/_  __/ ____/   |  /  |/  /                          
+  \__ \ / / / __/ / /| | / /|_/ /                           
+ ___/ // / / /___/ ___ |/ /  / /                            
+/____//_/_/_____/_/ _|_/_/  /_/
+    __  __ ____  __  __ __     ____  ____  ____  ___________
+   / / / / __ \/ / / / __ \   / __ )/ __ \/ __ \/ ___/_  __/
+  / /_/ / / / / / / / /_/ /  / __  / / / / / / /\__ \ / /   
+ / __  / /_/ / /_/ / _, _/  / /_/ / /_/ / /_/ /___/ // /    
+/_/ /_/\____/\____/_/ |_|  /_____/\____/\____//____//_/   
+
+"""
+
+client = SteamClient()
+
+def login():
+    print(ascii_art)
+    user,passw = str(input('[STB] Username: ')), getpass("[STB] Password: ")
+    account_login = client.login(username=user, password=passw)
+    if str(account_login) == "EResult.AccountLoginDeniedNeedTwoFactor":
+        system('cls')
+        login_with_steam_guard(user, passw)
+        main(user, passw)
+    elif str(account_login) == "EResult.AccountLogonDenied":
+        system('cls')
+        login_with_email_auth_code(user, passw)
+        main(user, passw)
+    elif str(account_login) == "EResult.InvalidPassword":
+        system('cls')
+        print('[ERROR] Invalid Login or Password.')
+        login()
+    elif str(account_login) == "EResult.OK":
+        main(user, passw)
+    else:
+        print("[ERROR]: " + str(account_login))
+        system('pause')
+        sys.exit()
+
+def login_with_email_auth_code(username, password):
+    print(ascii_art)
+    code = str(input('[STB] Steam Guard Code (sended to your email): '))
+    account_login = client.login(username=username, password=password, auth_code=code)
+    if str(account_login) == "EResult.InvalidPassword":
+        system('cls')
+        print('[STB] Wrong Password\n')
+        login()
+    elif str(account_login) == "EResult.OK":
+        system('cls')
+        main()
+    elif str(account_login) == "EResult.InvalidLoginAuthCode":
+        system('cls')
+        print('[ERROR] Invalid Steam Guard Code.')
+        login_with_email_auth_code(username, password)
+    else:
+        print("[ERROR]: " + str(account_login))
+        system('pause')
+        sys.exit()
+
+
+def login_with_steam_guard(username, password):
+    print(ascii_art)
+    code = str(input('[STB] Steam Guard Code: '))
+    account_login = client.login(username=username, password=password, two_factor_code=code)
+    if str(account_login) == "EResult.InvalidPassword":
+        system('cls')
+        print('[STB] Wrong Password\n')
+        login()
+    elif str(account_login) == "EResult.OK":
+        system('cls')
+        main()
+    elif str(account_login) == "EResult.TwoFactorCodeMismatch":
+        system('cls')
+        print('[ERROR] Invalid Steam Guard Code.')
+        login_with_steam_guard(username, password)
+    else:
+        print("[ERROR]: " + str(account_login))
+        system('pause')
+        sys.exit()
+
+def main():
+    print(ascii_art)
+    print(f'[STB] Logged in as {client.user.name}')
+    try:
+        game_id = int(input('[STB] Game ID: '))
+        custom_game_name = str(input('[STB] Custom Game Name: '))
+        if game_id != "":
+            system('cls')
+            print(ascii_art)
+            get_game_name = req.get(f"https://store.steampowered.com/api/appdetails/?appids={game_id}&filters=basic").json()
+            if get_game_name == "null":
+                system("cls")
+                print(f'[STB] {game_id} its not a valid game id.')
+                main()
+            elif get_game_name[str(game_id)]["success"] == False:
+                system("cls")
+                print(f'[STB] {game_id} its not a valid game id.')
+                main()
+            else:
+                game_name = str(get_game_name[str(game_id)]["data"]["name"])
+
+            client.send(MsgProto(EMsg.ClientGamesPlayed), {'games_played': [{'game_id': game_id, 'game_extra_info': custom_game_name},]})
+            system(f"title Steam Hour Boost - @flooowd - Running {game_name}")
+            print(f'[STB] Game: {game_name} | Status: Running')
+            client.run_forever()
+        else:
+            system('cls')
+            print("[STB] Game ID can't be empty.")
+            main()
+    except (ValueError):
+        system('cls')
+        print('[STB] Game ID needs to be int.')
+        main()
+if __name__ in "__main__":
+    system("cls && color c && title Steam Hour Boost - @flooowd")
+    login()
